a19f2cc5b8061d8008a5cd664d478cf9ac0d9b3d
黑岸 <oncwnuHM1_UwSYXbeIPs6OYsHrYI@git.weixin.qq.com>
2024-04-18 15:10:43 +0800

    实现登陆验证中间件

--- server/app/router.js
+++ server/app/router.js
@@ -5,4 +5,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.post('/api/user/register', controller.user.register);
   router.post('/api/user/login', controller.user.login);
+  router.get('/api/user/test', controller.user.test);
+
 };

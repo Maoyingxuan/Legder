408b2b4c0740d8065a489e41d570e965060e1a4f
黑岸 <oncwnuHM1_UwSYXbeIPs6OYsHrYI@git.weixin.qq.com>
2024-04-21 00:21:27 +0800

    less引入

--- client/src/container/Index/index.jsx
+++ client/src/container/Index/index.jsx
@@ -1,9 +1,14 @@
 // Index/index.jsx
 // import React from 'react'
 import { Button } from 'zarm'
-export default function Index() {
-    return <div>
-    Index
-    <Button theme='primary'>按钮</Button>
-  </div>
+
+import s from './style.module.less'
+
+export default function Index(){
+    return(
+        <div className={s.index}>
+            <span>样式</span>
+        <Button theme='primary'>按钮</Button>
+        </div>
+    )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'dashesOnly'
+    },
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      }
+    }
+  },
 })

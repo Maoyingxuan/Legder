ac972d2295cc74b36f9d134506355ef7f30943d0
黑岸 <oncwnuHM1_UwSYXbeIPs6OYsHrYI@git.weixin.qq.com>
2024-04-21 23:50:58 +0800

    实现登录页面

--- client/src/container/Login/index.jsx
+++ client/src/container/Login/index.jsx
@@ -3,16 +3,18 @@ import { useState,useCallback } from 'react';
 import { List, Input, Checkbox,Button,Toast} from 'zarm'
 import { post } from '@/utils'
 import Captcha from "react-captcha-code"
+import cx from 'classnames'
 const Login = () => {
     const [username, setUsername] = useState(''); // 账号
     const [password, setPassword] = useState(''); // 密码
     const [verify, setVerify] = useState(''); // 验证码
     const [captcha, setCaptcha] = useState(''); // 验证码变化后存储值
-//  验证码变化，回调方法
+    const [type,setType] = useState('login') //类型切换（注册or登录）
+  //  验证码变化，回调方法
     const handleChange = useCallback((captcha) => {
-  console.log('captcha', captcha)
-  setCaptcha(captcha)
-}, []);
+    console.log('captcha', captcha)
+    setCaptcha(captcha)
+   }, []);
     const onSubmit = async () => {
         // console.log(username)
         if (!username) {
@@ -23,20 +25,34 @@ const Login = () => {
           Toast.show('请输入密码')
           return
         }
-        if (!verify) {
-          Toast.show('请输入验证码')
-          return
-        }
-        if (verify != captcha) {
-          Toast.show('验证码错误')
-          return
-        }
         try {
-          const { data } = await post('/user/register', {
-            username,
-            password
-          });
-          Toast.show('注册成功');
+          // 判断是否是登录状态
+          if (type == 'login') {
+            // 执行登录接口，获取 token
+            const { data } = await post('/user/login', {
+              username,
+              password
+            });
+            // 将 token 写入 localStorage
+            console.log("success")
+            localStorage.setItem('token', data.token);
+          } else {
+            if (!verify) {
+              Toast.show('请输入验证码')
+              return
+            }
+            if (verify != captcha) {
+              Toast.show('验证码错误')
+              return
+            }
+           await post('/user/register', {
+              username,
+              password
+            });
+            Toast.show('注册成功');
+            // 注册成功，自动将 tab 切换到 login 状态
+            setType('login');
+          }
         } catch (error) {
           Toast.show('系统错误');
         }
@@ -44,7 +60,8 @@ const Login = () => {
     return <div className={s.auth}>
     <div className={s.head}></div>
     <div className={s.tab}>
-        <span>注册</span>
+    <span className={cx({ [s.avtive]: type == 'login' })} onClick={() => setType('login')}>登录</span>
+      <span className={cx({ [s.avtive]: type == 'register' })} onClick={() => setType('register')}>注册</span>
     </div>
     <div className={s.form}>
     <List>
@@ -58,7 +75,7 @@ const Login = () => {
         <Checkbox />
         <label className="text-light">阅读并同意<a>《毛毛条款》</a></label>
       </div>
-      <Button onClick={onSubmit} block theme="primary">注册</Button>
+      <Button onClick={onSubmit} block theme="primary">{type == 'login' ? '登录' : '注册'}</Button>
     </div>
   </div>
 }
